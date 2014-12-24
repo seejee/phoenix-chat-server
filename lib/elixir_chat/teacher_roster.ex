6fbe3d0e2593e8db9b0bb10cd2e5d34b03f622fd
Chris Geihsler <chris@geihsler.net>
2014-12-24 05:05:51 -0500

    Converted teacher roster to use a HashDict.

--- lib/elixir_chat/teacher_roster.ex
+++ lib/elixir_chat/teacher_roster.ex
@@ -1,25 +1,20 @@
 defmodule ElixirChat.TeacherRoster do
   def new do
-    []
+    HashDict.new
   end
 
   def add(roster, teacher) do
     teacher = Map.merge(teacher, %{student_ids: []})
 
-    unless exists?(roster, teacher) do
-      roster = roster ++ [teacher]
-    end
-
-    roster
+    Dict.put(roster, teacher.id, teacher)
   end
 
   def remove(roster, teacher) do
-    roster -- [teacher]
+    Dict.delete(roster, teacher.id)
   end
 
   def claim_student(roster, teacher_id, student_id) do
-    index  = Enum.find_index(roster, fn(t) -> t.id == teacher_id end)
-    List.update_at(roster, index, fn(t) -> set_student_on_teacher(t, student_id) end)
+    Dict.update!(roster, teacher_id, fn(t) -> set_student_on_teacher(t, student_id) end)
   end
 
   def set_student_on_teacher(teacher, student_id) do
@@ -27,20 +22,15 @@ defmodule ElixirChat.TeacherRoster do
   end
 
   def chat_finished(roster, teacher_id, student_id) do
-    index  = Enum.find_index(roster, fn(t) -> t.id == teacher_id end)
-    List.update_at(roster, index, fn(t) -> remove_student(t, student_id) end)
+    Dict.update!(roster, teacher_id, fn(t) -> remove_student(t, student_id) end)
   end
 
   def remove_student(teacher, student_id) do
     Map.merge(teacher, %{student_ids: teacher.student_ids -- [student_id]})
   end
 
-  def exists?(roster, teacher) do
-    Enum.any?(roster, fn(t) -> t.id == teacher.id end)
-  end
-
   def find(roster, teacher_id) do
-    Enum.find(roster, fn(t) -> t.id == teacher_id end)
+    Dict.get(roster, teacher_id)
   end
 
   def can_accept_more_students?(roster, teacher) do
@@ -52,9 +42,10 @@ defmodule ElixirChat.TeacherRoster do
   end
 
   def stats(roster) do
+    teachers = Dict.values(roster)
+
     %{
-      total: length(roster),
+      total: length(teachers),
     }
   end
 end
-
